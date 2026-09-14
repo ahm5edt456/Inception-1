21259a3ef4b57ed1cb558df330b7acecc0880d8a
Ahmed <ahmad7raed77@gmail.com>
2026-09-14 20:05:42 +0300

    Update mariadb-init.sh

--- srcs/requirements/mariadb/tools/mariadb-init.sh
+++ srcs/requirements/mariadb/tools/mariadb-init.sh
@@ -1,1 +1,17 @@
-#!/bin/
+#!/bin/sh
+
+set -eu
+
+PATH_MARIADB="/var/lib/mysql/"
+PATH_RUNTIME="/run/mysqld/"
+PATH_SOCKET="$PATH_RUNTIME/mysqld.sock"
+
+mkdir -p "$PATH_RUNTIME"
+chown -R mysql:mysql "$PATH_RUNTIME"
+
+if [[ ! -d "$PATH_MARIADB/mysql/" ]]; then
+  echo "Initializing MariaDB system tables..."
+
+  mariadb-install-db \
+  --user=mysql \
+  --datedir="$PATH_MARIADB"
